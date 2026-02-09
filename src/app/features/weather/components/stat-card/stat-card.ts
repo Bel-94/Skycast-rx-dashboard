f13b5dafb5ea8cd2b286ee444c85e32a17134e7c
Bel-94 <ntinyaribelinda@gmail.com>
2026-02-09 11:46:23 +0300

    feat: make statcard reusabl by using input

--- src/app/features/weather/components/stat-card/stat-card.ts
+++ src/app/features/weather/components/stat-card/stat-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stat-card',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './stat-card.css',
 })
 export class StatCard {
+  @Input() label = '';
+  @Input() value = '';
+  @Input() icon = '';
 
 }
