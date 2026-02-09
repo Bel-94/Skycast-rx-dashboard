4885e6462001d5e72162dcbc990052247780eef3
Bel-94 <ntinyaribelinda@gmail.com>
2026-02-09 11:15:46 +0300

    feat: Make WeatherDisplay my main screen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherDisplay } from './features/weather/components/weather-display/weather-display';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WeatherDisplay],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
