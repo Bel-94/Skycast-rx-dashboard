c39e4c42d744d496885abfd70ec10d31f7438943
Bel-94 <ntinyaribelinda@gmail.com>
2026-02-09 13:26:29 +0300

    feat: Make SearchComponent reactive

--- src/app/features/weather/components/search/search.ts
+++ src/app/features/weather/components/search/search.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
+   @Output() cityChange = new EventEmitter<string>();
+
+  city = new FormControl('', { nonNullable: true });
+
+  constructor() {
+    this.city.valueChanges.pipe(
+      map(v => v.trim()),
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter(v => v.length >= 2),
+    ).subscribe(v => this.cityChange.emit(v));
+  }
 
 }
